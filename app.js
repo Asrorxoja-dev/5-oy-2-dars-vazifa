dae065d17e5c69da1a7b21548c242f0e9642c521
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2024-01-14 12:30:34 +0500

    vazifa bajarildi

---
+++ app.js
@@ -0,0 +1,72 @@
+// 1-masala
+
+// let arr = ['hello ','bye'];
+// let [a, b ] = arr;
+// console.log(arr);
+
+
+
+// 2-masala
+
+
+
+
+
+
+
+// 3-masala
+
+// let arr = [1,2,3,4,5,];
+// let [a, b, c,d,i] = arr;
+
+// console.log(a, b);
+// console.log(c,d,i);
+
+
+
+// 4-masala
+
+// let user = { person: { name: 'John', surname:'Doe', age: 30}}
+
+
+// console.log(user.person.name);
+// console.log(user.person.age);
+
+
+
+// 5-masala
+ 
+
+// const user = {
+//     name: "Ali",
+//     age: 25
+//   };
+  
+//   const {name, age, city = "Fergana", country = "O'zbekiston"} = user;
+  
+//   console.log(name); 
+//   console.log(age); 
+//   console.log(city); 
+//   console.log(country); 
+
+
+
+
+// 6-masala
+
+  
+  
+//     const user ={
+//     name: "Ali",
+//     age: 25,
+//     city : "Fergana",
+//     country : "O'zbekiston"
+//   };
+  
+//   const user1= Object.entries(user);
+//   console.log(user1);
+  
+
+
+
+
